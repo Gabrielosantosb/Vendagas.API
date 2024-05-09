da2ff5944879ab72adc876b07958355ca7ca6cf8
Gabriel Santos <gabrielosantosb@outlook.com>
2024-05-09 19:57:21 -0300

    TokenService

---
+++ Vendagas.API/Application/Services/Token/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using Vendagas.API.ORM.Entity;
+
+namespace Vendagas.API.Application.Services.Token
+{
+    public interface ITokenService
+    {
+        string GenerateToken(string key, string issuer, string audience, UserModel profissional);
+        string? GetUserEmail();
+        int GetUserId();
+
+    }
+}
--- Vendagas.API/ORM/Entity/UserModel.cs
+++ Vendagas.API/ORM/Entity/UserModel.cs
@@ -6,7 +6,7 @@ namespace Vendagas.API.ORM.Entity
     {
         [Key]
         public int UserId{ get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
